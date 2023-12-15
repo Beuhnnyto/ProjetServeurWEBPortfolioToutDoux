b7b9d3f60b29661eee9140ed909ea2d1730d2640
Beuhnnyto <adam.malek@efrei.net>
2023-12-15 14:01:10 +0100

    AHHHHHHH

--- todo/api.js
+++ todo/api.js
@@ -2,7 +2,9 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const mysql = require('mysql');
-var ports = [4000,4001,4002,4003,4004,4005,4006,4007,4008,4009]
+
+var ports = [4000, 4001, 4002, 4003, 4004, 4005, 4006, 4007, 4008, 4009];
+
 //use express
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -14,7 +16,6 @@ const connection = mysql.createConnection({
   password: '',
   database: 'tasks.sql'
 });
-
 
 // Connect to the MySQL database
 connection.connect((err) => {
@@ -32,14 +33,43 @@ app.get('/', (_, res) => {
       console.error('Error connecting to MySQL database:', err);
       return;
     }
-    res.render('index.html', { tasks: results });
+    let tableRows = '';
+    results.forEach(task => {
+      tableRows += `<tr><td>${task.title}</td><td>${task.description}</td><td>
+        <form action="/${task.id}" method="POST">
+          <input type="checkbox" name="done" onchange="this.form.submit()" ${task.done ? 'checked' : ''}>
+        </form>
+
+      </td></tr>`;
+    });
+    const table = `<table><tr><th>Title</th><th>Description</th><th>Done</th></tr>${tableRows}</table>`;
+    
+    const html = `
+      <html>
+        <head>
+          <title>Tasks</title>
+        </head>
+        <body>
+          <h1>Tasks</h1>
+          ${table}
+          <form action="/" method="POST">
+            <input type="text" name="title" placeholder="Title" required>
+            <input type="text" name="description" placeholder="Description" required>
+            <input type="submit" value="Add task">
+          </form>
+        </body>
+      </html>
+    `;
+    res.send(html);
+
+
+
   });
 });
 
-app.post('/tasks', (req, res) => {
+app.post('/', (req, res) => {
   console.log(req.body);
-  title = req.body.title;
-  description = req.body.description;
+  const { title, description } = req.body;
 
   const query = `INSERT INTO tasks (title, description, done) VALUES ('${title}', '${description}', 0)`;
   connection.query(query, (err, results) => {
@@ -47,9 +77,26 @@ app.post('/tasks', (req, res) => {
       console.error('Error connecting to MySQL database:', err);
       return;
     }
-    res.send(results);
+    
+    res.redirect('/');
   });
 });
+
+app.post('/:id', (req, res) => {
+  const { id } = req.params;
+  const { done } = req.body;
+  const query = `UPDATE tasks SET done = ${done ? 1 : 0} WHERE id = ${id}`;
+  connection.query(query, (err, results) => {
+    if (err) {
+      console.error('Error connecting to MySQL database:', err);
+      return;
+    }
+    
+    res.redirect('/');
+  });
+}
+);
+
 
 ports.forEach(port => {
   app.listen(port, () => {
